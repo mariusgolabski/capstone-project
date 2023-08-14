c49a1737079e430aff7eb3c0bc70fc926bb589e3
Marius Golabski <mgolabski@gmail.com>
2023-08-14 22:59:51 +0200

    add some styling

---
+++ components/InterviewSection/InterviewSection.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+  margin: 0 auto;
+  max-width: 840px;
+  display: grid;
+  grid-gap: 1.5rem;
+`;
---
+++ components/layout/layout.styled.js
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const StyledMain = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1.5rem;
+`;
