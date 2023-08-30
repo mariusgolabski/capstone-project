9d497ac2d2010c8f263423650e7cc7ed6ee06f57
Marius Golabski <mgolabski@gmail.com>
2023-08-30 23:46:24 +0200

    style profile tab navigation wrapper

---
+++ components/ProfileTabNavWrapper/ProfileTabNavWrapper.Styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ProfileTabNavWrapper = styled.div`
+  padding-top: 3rem;
+  background-color: #fff;
+  box-shadow: 0 1px 2px rgba(234, 234, 234, 1);
+`;
