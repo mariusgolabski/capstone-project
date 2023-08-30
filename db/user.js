e34359638cf6955718c7ab9d8a9b0e549c5d6a0a
Marius Golabski <mgolabski@gmail.com>
2023-08-30 23:43:12 +0200

    add user data file

---
+++ db/user.js
@@ -0,0 +1,9 @@
+export const user = {
+  id: "1",
+  firstName: "John",
+  lastName: "Doe",
+  jobTitle: "Director data engineering",
+  companyName: "NovaTech Solutions",
+  userCoverImagePath: "/bg3.jpg",
+  userProfileImagePath: "/avatar.png",
+};
