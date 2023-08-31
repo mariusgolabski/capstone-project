bea9cb5ac8f23e6b642ab5f66549fcb334c6df1f
Marius Golabski <mgolabski@gmail.com>
2023-08-31 21:56:42 +0200

    integrate SWR to fetch and display user data in the ProfileHeader component

--- components/ProfileHeader/index.js
+++ components/ProfileHeader/index.js
@@ -1,4 +1,5 @@
-import { user } from "../../db/user";
+// import { user } from "../../db/user";
+import useSWR from "swr";
 
 import {
   StyledWrapper,
@@ -13,13 +14,29 @@ import {
 } from "./ProfileHeader.styled";
 
 export default function ProfileHeader() {
+  const { data, error, isLoading } = useSWR(
+    "/api/users/64f0c5a8b979a78d64d3b750"
+  );
+
+  if (error) {
+    return <div>Failed to load user data.</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data) {
+    return <div>No user data available.</div>;
+  }
+
   return (
     <StyledWrapper>
       <StyledProfileHeaderWrapper>
         <SyledProfileCoverWrapper>
           <StyledCoverImage
             priority={true}
-            src={user.userCoverImagePath}
+            src={data.userCoverImagePath}
             alt="profile-cover"
             width={1110}
             height={160}
@@ -28,15 +45,15 @@ export default function ProfileHeader() {
         <StyledUserProfileCard>
           <StyledUserProfileImageWrapper>
             <StyledUserProfileImage
-              src={user.userProfileImagePath}
+              src={data.userProfileImagePath}
               alt="avatar"
               width={168}
               height={168}
             />
           </StyledUserProfileImageWrapper>
           <StyledProfileInfo>
-            <h1>{`${user.firstName} ${user.lastName}`}</h1>
-            <StyledDescriptionText>{`${user.jobTitle} @${user.companyName}`}</StyledDescriptionText>
+            <h1>{`${data.firstName} ${data.lastName}`}</h1>
+            <StyledDescriptionText>{`${data.jobTitle} @${data.companyName}`}</StyledDescriptionText>
           </StyledProfileInfo>
         </StyledUserProfileCard>
       </StyledProfileHeaderWrapper>
