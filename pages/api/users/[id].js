81403b1721868df145e84e3067efff1abdd8e7c7
Marius Golabski <mgolabski@gmail.com>
2023-09-03 18:07:40 +0200

    format

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -4,14 +4,11 @@ import User from "../../../db/models/User";
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
-
   if (request.method === "GET") {
     const user = await User.findById(id);
-
     if (!user) {
       return response.status(404).json({ status: "Not Found" });
     }
-
     response.status(200).json(user);
   }
 }
