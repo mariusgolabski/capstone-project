46f6839cc8670b7910721a5f96679f5cff3ad525
Marius Golabski <mgolabski@gmail.com>
2023-09-04 22:30:12 +0200

    cleanup

--- db/models/User.js
+++ db/models/User.js
@@ -3,21 +3,16 @@ const { Schema } = mongoose;
 
 mongoose.set("debug", true);
 
-const userSchema = new Schema(
-  {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    jobTitle: { type: String, required: true },
-    companyName: { type: String, required: true },
-    userCoverImagePath: { type: String, required: true },
-    userProfileImagePath: { type: String, required: true },
-    email: { type: String, required: true },
-  },
-  {
-    collection: "users",
-  }
-);
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  jobTitle: { type: String, required: true },
+  companyName: { type: String, required: true },
+  userCoverImagePath: { type: String, required: true },
+  userProfileImagePath: { type: String, required: true },
+  email: { type: String, required: true },
+});
 
-const User = mongoose.models.user || mongoose.model("user", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
