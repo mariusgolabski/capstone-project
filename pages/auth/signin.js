7ab19e007bb149a2cf4cdfe13673cca611686b15
Marius Golabski <mgolabski@gmail.com>
2023-09-07 16:22:40 +0200

    custom fake-login

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,4 +1,5 @@
-import { signIn, getProviders } from "next-auth/react";
+import { signIn, getProviders, getCsrfToken } from "next-auth/react";
+import { useState } from "react";
 import Header from "../../components/Header";
 import {
   StyledButton,
@@ -11,7 +12,14 @@ import {
   Wrapper,
 } from "../../components/SignIn/SignIn.styled";
 
-export default function Signin({ providers }) {
+export default function Signin({ csrfToken, providers }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleFakeLogin() {
+    signIn("credentials", { username, password });
+  }
+
   return (
     <>
       <Header />
@@ -21,23 +29,47 @@ export default function Signin({ providers }) {
           {providers &&
             Object.values(providers).map((provider) => (
               <div key={provider.name}>
-                <StyledButton onClick={() => signIn()}>
+                <StyledButton onClick={() => signIn(provider.id)}>
                   Sign in with {provider.name}
                 </StyledButton>
               </div>
             ))}
           <StyledDivider />
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <StyledLabel htmlFor="Username">Username</StyledLabel>
+          <StyledInput
+            id="Username"
+            type="text"
+            placeholder="testuser"
+            required
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <StyledLabel htmlFor="Password">Password</StyledLabel>
+          <StyledInput
+            id="password"
+            type="password"
+            placeholder="testuser"
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <StyledLoginButton onClick={handleFakeLogin}>
+            Submit
+          </StyledLoginButton>
         </StyledLogin>
       </Wrapper>
     </>
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   const providers = await getProviders();
+  const csrfToken = await getCsrfToken(context);
   return {
     props: {
       providers,
+      csrfToken,
     },
   };
 }
