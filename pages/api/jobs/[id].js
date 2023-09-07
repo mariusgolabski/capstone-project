62efc2ffa90ca65582415e6bce4dca2627538215
Marius Golabski <mgolabski@gmail.com>
2023-09-07 13:38:05 +0200

    refactor job API to use user ID from query parameters in GET requests

--- pages/api/jobs/[id].js
+++ pages/api/jobs/[id].js
@@ -3,20 +3,38 @@ import Job from "@/db/models/Job";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id } = request.query;
+  const { id: user_id } = request.query;
   const jobData = request.body;
-  if (request.method === "PUT") {
+
+  if (request.method === "GET") {
+    try {
+      const userJobs = await Job.find({
+        user_id: user_id,
+      })
+        .populate("mustHaveSkills")
+        .populate("niceToHaveSkills");
+
+      response.status(200).json(userJobs);
+    } catch (error) {
+      response
+        .status(500)
+        .json({ error: "Internal server error while fetching jobs." });
+    }
+  } else if (request.method === "PUT") {
+    const { id } = request.query;
+
     try {
       const updatedJob = await Job.findByIdAndUpdate(id, jobData);
 
       if (!updatedJob) {
-        return response.status(404).json({ error: "Interview not found." });
+        return response.status(404).json({ error: "Job not found." });
       }
       return response.status(200).json({ message: "Job updated successfully" });
     } catch (error) {
       return response.status(500).json({ error: "Internal server error" });
     }
   } else if (request.method === "DELETE") {
+    const { id } = request.query;
     await Job.findByIdAndDelete(id);
     response.status(200).json({ message: "Job successfully deleted." });
   } else {
