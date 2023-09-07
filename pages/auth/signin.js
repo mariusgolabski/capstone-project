e353ab9247d7b8f7935732bda297d7178d0fe14e
Marius Golabski <mgolabski@gmail.com>
2023-09-07 15:47:54 +0200

    update signIn callback

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,4 +1,4 @@
-import { signIn, getCsrfToken, getProviders } from "next-auth/react";
+import { signIn, getProviders } from "next-auth/react";
 import Header from "../../components/Header";
 import {
   StyledButton,
@@ -11,7 +11,7 @@ import {
   Wrapper,
 } from "../../components/SignIn/SignIn.styled";
 
-export default function Signin({ csrfToken, providers }) {
+export default function Signin({ providers }) {
   return (
     <>
       <Header />
@@ -21,47 +21,23 @@ export default function Signin({ csrfToken, providers }) {
           {providers &&
             Object.values(providers).map((provider) => (
               <div key={provider.name}>
-                <StyledButton
-                  onClick={() =>
-                    signIn(provider.id, {
-                      callbackUrl: "http://localhost:3000/home",
-                    })
-                  }
-                >
+                <StyledButton onClick={() => signIn(provider.id)}>
                   Sign in with {provider.name}
                 </StyledButton>
               </div>
             ))}
           <StyledDivider />
-          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <StyledLabel htmlFor="email">Email</StyledLabel>
-          <StyledInput
-            id="email"
-            type="email"
-            placeholder="Enter email address"
-            required
-          />
-          <StyledLabel htmlFor="password">password</StyledLabel>
-          <StyledInput
-            id="password"
-            type="password"
-            placeholder="Enter password"
-            required
-          />
-          <StyledLoginButton>Submit</StyledLoginButton>
         </StyledLogin>
       </Wrapper>
     </>
   );
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
   const providers = await getProviders();
-  const csrfToken = await getCsrfToken(context);
   return {
     props: {
       providers,
-      csrfToken,
     },
   };
 }
