2faa4bafeec2a22f6b2d0b6a6bbe512b31245d1a
Marius Golabski <mgolabski@gmail.com>
2023-09-07 13:46:32 +0200

    updated JobsSection to accept (session)userId prop and fetch job data dynamically

--- components/JobsSection/index.js
+++ components/JobsSection/index.js
@@ -1,17 +1,45 @@
+import useSWR from "swr";
+
 import {
   AddJobSvg,
   StyledOpenModalButton,
   StyledSection,
 } from "./JobsSection.styled";
 import Job from "../Job";
-export default function JobsSection({ jobs, openJobModal, onEdit, onDelete }) {
+export default function JobsSection({
+  userId,
+  openJobModal,
+  onEdit,
+  onDelete,
+}) {
+  const { data: jobs, error, isLoading } = useSWR(`/api/jobs/${userId}`);
+
+  if (error) {
+    return <p>Failed to load job data.</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!jobs) {
+    return <p>No job data available.</p>;
+  }
+
   return (
     <StyledSection>
       <StyledOpenModalButton onClick={openJobModal} aria-label="Add Job">
         <AddJobSvg />
       </StyledOpenModalButton>
       {jobs.map((job) => (
-        <Job job={job} key={job._id} onEdit={onEdit} onDelete={onDelete} />
+        <Job
+          userId={userId}
+          job={job}
+          key={job._id}
+          onEdit={onEdit}
+          // onDelete={onDelete}
+          onDelete={() => onDelete(job, "jobs")}
+        />
       ))}
     </StyledSection>
   );
