ad2dc2579083d6b62d15f6e84bfb1a0e98fe45e5
Marius Golabski <mgolabski@gmail.com>
2023-09-08 14:31:33 +0200

    refactor interview API endpoint to use session for user authentication

--- pages/api/interviews/[id].js
+++ pages/api/interviews/[id].js
@@ -1,17 +1,31 @@
 import dbConnect from "@/db/connect";
 import Interview from "@/db/models/Interview";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id: userId } = request.query;
+
+  const { id: interviewId } = request.query;
+
+  const session = await getServerSession(request, response, authOptions);
+
+  if (!session) {
+    return response.status(401).json({ message: "Unauthorized" });
+  }
+
+  const sessionUserId = session.user.id;
+
+  // check
+
   if (request.method === "GET") {
     try {
       const userInterviews = await Interview.find({
-        userId: userId,
+        userId: sessionUserId,
       })
         .populate({
           path: "category",
-          select: "-questions", // Exclude the questions field
+          select: "-questions",
         })
         .populate("question")
         .populate("answer");
@@ -23,11 +37,22 @@ export default async function handler(request, response) {
         .json({ error: "Internal server error while fetching interviews." });
     }
   } else if (request.method === "PUT") {
-    const { id } = request.query;
     const interviewData = request.body;
+
     try {
+      const interview = await Interview.findById(interviewId);
+
+      if (!interview) {
+        return response.status(404).json({ error: "Interview not found." });
+      }
+      // https://www.mongodb.com/docs/manual/reference/method/ObjectId.toString/
+      if (interview.userId.toString() !== sessionUserId) {
+        return response
+          .status(401)
+          .json({ message: "Unauthorized, wrong user." });
+      }
       const updatedInterview = await Interview.findByIdAndUpdate(
-        id,
+        interviewId,
         interviewData
       );
       if (!updatedInterview) {
@@ -42,10 +67,47 @@ export default async function handler(request, response) {
         .json({ error: "Internal server error while updating interview." });
     }
   } else if (request.method === "DELETE") {
-    const { id } = request.query;
-    await Interview.findByIdAndDelete(id);
-    response.status(200).json({ message: "Interview successfully deleted." });
+    try {
+      const interview = await Interview.findById(interviewId);
+
+      if (!interview) {
+        return response.status(404).json({ error: "Interview not found." });
+      }
+      if (interview.userId.toString() !== sessionUserId) {
+        return response
+          .status(401)
+          .json({ message: "Unauthorized, wrong user." });
+      }
+
+      // Delete the interview
+      await Interview.findByIdAndDelete(interviewId);
+      return response
+        .status(200)
+        .json({ message: "Interview successfully deleted." });
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ error: "Internal server error while deleting interview." });
+    }
+  } else if (request.method === "POST") {
+    try {
+      const interviewData = {
+        ...request.body, // spread existing interview data
+        userId: sessionUserId, // add sessionUserId
+      };
+
+      const interview = new Interview(interviewData);
+      await interview.save();
+
+      return response
+        .status(201)
+        .json({ message: "Interview successfully created" });
+    } catch (error) {
+      return response
+        .status(400)
+        .json({ error: "Failed to create interview", message: error.message });
+    }
   } else {
-    response.status(405).json({ error: "Method Not Allowed" });
+    return response.status(405).end("Method Not Allowed");
   }
 }
