7d18df6e2e11876be4580f760c80c2311ba0d56a
Marius Golabski <mgolabski@gmail.com>
2023-09-10 17:37:12 +0200

    updated API handler for job details retrieval via GET request

--- pages/api/jobs/[id].js
+++ pages/api/jobs/[id].js
@@ -15,7 +15,24 @@ export default async function handler(request, response) {
 
   const sessionUserId = session.user.id;
 
-  if (request.method === "PUT") {
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    try {
+      const job = await Job.findById(id)
+        .populate("mustHaveSkills")
+        .populate("niceToHaveSkills")
+        .populate("user_id", "userProfileImagePath");
+
+      if (!job) {
+        return response.status(404).json({ message: "Job not found" });
+      }
+
+      return response.status(200).json(job);
+    } catch (error) {
+      return response.status(500).json({ message: "Internal Server Error" });
+    }
+  } else if (request.method === "PUT") {
     const { id: _id } = request.query;
     try {
       const job = await Job.findById(_id);
