978776cf3254a482f0a9a3f2c9e6148d0f382901
Marius Golabski <mgolabski@gmail.com>
2023-09-11 02:07:14 +0200

    create interview and job component

---
+++ components/LeaderDetails/Job/index.js
@@ -0,0 +1,43 @@
+import { Avatar } from "../../Avatar";
+
+import {
+  StyledCard,
+  StyledCompanyName,
+  StyledFooter,
+  StyledSkillsWrapper,
+  StyledSkill,
+  StyledContentWrapper,
+  StyledList,
+  StyledListItem,
+  StyledAvatarWrapper,
+} from "../Job/Job.styled";
+
+export default function Job({ job, userProfileImagePath }) {
+  console.log(job);
+
+  return (
+    <StyledCard>
+      <StyledContentWrapper>
+        <StyledAvatarWrapper>
+          <Avatar userImage={userProfileImagePath} />
+          <StyledCompanyName>{job.companyName}</StyledCompanyName>
+        </StyledAvatarWrapper>
+        <h2>{job.jobTitle}</h2>
+        <p>{`€ ${job.annualSalaryRange[0]} - ${job.annualSalaryRange[1]} K`}</p>
+        <StyledSkillsWrapper>
+          {job.mustHaveSkills.map((skill) => (
+            <StyledSkill key={skill.value}>{skill.value}</StyledSkill>
+          ))}
+        </StyledSkillsWrapper>
+      </StyledContentWrapper>
+
+      <StyledFooter>
+        <StyledList>
+          <StyledListItem>{job.location}</StyledListItem>
+          <StyledListItem>{job.employmentType}</StyledListItem>
+          <StyledListItem>{job.seniorityLevel}</StyledListItem>
+        </StyledList>
+      </StyledFooter>
+    </StyledCard>
+  );
+}
