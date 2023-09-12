d1c53013abce3b02e12f029db32bc373649c28fb
Marius Golabski <mgolabski@gmail.com>
2023-09-12 13:29:41 +0200

    add author

--- components/JobDetails/index.js
+++ components/JobDetails/index.js
@@ -1,6 +1,9 @@
+import { createKey } from "next/dist/shared/lib/router/router";
 import {
   StyledApplyButton,
+  StyledAvatarWrapper,
   StyledClock,
+  StyledCompanyName,
   StyledDetailsSection,
   StyledEuro,
   StyledGlobe,
@@ -14,11 +17,14 @@ import {
   StyledSkill,
   StyledSkillsTitle,
   StyledSkillsWrapper,
+  StyledUserName,
 } from "./JobDetails.styled";
+
+import { Avatar } from "@/components/Avatar";
+import Link from "next/link";
 
 export default function JobDetails({ job }) {
   console.log(job);
-
   return (
     <StyledJobDetailsWrapper>
       <StyledJobCard>
@@ -51,6 +57,7 @@ export default function JobDetails({ job }) {
               {`${job.annualSalaryRange[0]} - ${job.annualSalaryRange[1]} K`}
             </StyledHeroFeature>
           </StyledHeroFeaturesContainer>
+
           <a href={job.howToApply} target="_blank" rel="noopener noreferrer">
             <StyledApplyButton>Apply</StyledApplyButton>
           </a>
@@ -69,6 +76,12 @@ export default function JobDetails({ job }) {
             ))}
           </StyledSkillsWrapper>
         </StyledDetailsSection>
+        <StyledAvatarWrapper>
+          <h2>Created by:</h2>
+          <Link href={`/leaders/${job.user_id._id}`}>
+            <Avatar userImage={job.user_id.userProfileImagePath} />
+          </Link>
+        </StyledAvatarWrapper>
       </StyledJobCard>
     </StyledJobDetailsWrapper>
   );
