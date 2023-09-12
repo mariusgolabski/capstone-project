73769c53789fed44fa41df2077905d65ff3ecd13
Marius Golabski <mgolabski@gmail.com>
2023-09-12 13:31:14 +0200

    add hero-section

--- pages/leaders/index.js
+++ pages/leaders/index.js
@@ -1,11 +1,15 @@
 import Header from "@/components/Header";
 import useSWR from "swr";
 import LeaderPreview from "@/components/LeaderPreview";
-import { StyledSection } from "@/components/LeaderPreview/LeaderPreview.styled";
+import {
+  StyledHeading,
+  StyledHeroDescription,
+  StyledLeaderHeroSection,
+  StyledSection,
+} from "@/components/LeaderPreview/LeaderPreview.styled";
 
 export default function Leaders() {
   const { data: leaders, error, isLoading } = useSWR(`/api/users`);
-  
 
   if (error) return <p>Faild to load</p>;
   if (isLoading) return <p>Loading...</p>;
@@ -13,6 +17,16 @@ export default function Leaders() {
   return (
     <>
       <Header />
+      <StyledLeaderHeroSection>
+        <StyledHeading>
+          The Minds Behind the Missions: Discover Your Next Leader
+        </StyledHeading>
+        <StyledHeroDescription>
+          {`
+         Leadership matters. That's why CuioHead offers you a unique chance to get to know your future leaders. Explore, connect, and find a mission that moves you."
+        `}
+        </StyledHeroDescription>
+      </StyledLeaderHeroSection>
       <StyledSection>
         {leaders.map((leader) => (
           <LeaderPreview key={leader._id} leader={leader} />
@@ -21,3 +35,5 @@ export default function Leaders() {
     </>
   );
 }
+
+("Connect With Leaders, Not Just Employers");
